2f1dc8d9ea5945320a29813b3d2a78a4b305b6eb
AramAvila <aram.avila91@gmail.com>
2016-03-09 12:29:10 +0100

    Bug fixes
    Optimized the method that creates points

--- JScanvas.js
+++ JScanvas.js
@@ -4,38 +4,33 @@ function flatCanvas(){
     var rows = $("#rows").val();
     var cols = $("#cols").val();
     var layers = $("#layers").val();
-    var length = $("#length").val();
+    var width = $("#width").val();
+    var height = $("#height").val();
 
-    var rowsSpacing = length / (rows-1);
-    var colsSpacing = length / (cols-1);
+    document.getElementById('myCanvas').width = width;
+    document.getElementById('myCanvas').height = height;
 
-    document.getElementById('myCanvas').width = length;
-    document.getElementById('myCanvas').height = length;
+    /*var axisX = new Point(1,0);
+    var axisY = new Point(0.707, 0.707);
+    var axisZ = new Point(0,1);*/
 
-    paper.project.activeLayer.removeChildren();    
+    var rowsSpacing = height / (rows-1);
+    var colsSpacing = width / (cols-1);
+
+    paper.project.activeLayer.removeChildren();
 
     for (var i = 0; i < rows; i++) {
-
         var path = new paper.Path();
         path.strokeColor = 'blue';
 
-        var start = new Point([i*rowsSpacing, 0]);
-        var end = new Point([i*rowsSpacing, length]);
-
-        path.add(start);
-        path.add(end);
+        path.add(new Point([0, i*rowsSpacing]), new Point([width/1, i*rowsSpacing])); //<-- Whithout dividing by 1 this wont work... can't understand why
     };
 
     for (var i = 0; i < cols; i++) {
-
         var path = new paper.Path();
         path.strokeColor = 'red';
 
-        var start = new Point([0, i*colsSpacing]);
-        var end = new Point([length, i*colsSpacing]);
-
-        path.add(start);
-        path.add(end);
+        path.add(new Point([i*colsSpacing, 0]), new Point([i*colsSpacing, height/1])); //<-- Whithout dividing by 1 this wont work... can't understand why
     };
 
     paper.view.draw();
@@ -47,17 +42,15 @@ $(document).ready(function(){
     // Get a reference to the canvas object
     var canvas = document.getElementById('myCanvas');
 
-    // Create an empty project and a view for the canvas:
     paper.setup(canvas);
-    // Create a Paper.js Path to draw a line into it:
 
     flatCanvas();
 
-    $("#rows, #cols, #layers, #length").keyup(function(){
+    $("#rows, #cols, #layers, #width, #height").keyup(function(){
         flatCanvas()
     });
 
-    $("#rows, #cols, #layers, #length").on("keydown", function(e){
+    $("#rows, #cols, #layers, #width, #height").on("keydown", function(e){
 
         switch(e.which) {
             case 38: // up
