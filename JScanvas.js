72f813b890ec82ceddf3ce8efd4d9fd72246db00
AramAvila <aram.avila91@gmail.com>
2016-03-12 17:08:00 +0100

    Gui completed
    Finished gui V1 and informative texts.

--- JScanvas.js
+++ JScanvas.js
@@ -26,12 +26,12 @@ var axisZ = new Point(0, 1) * multiplier;
 /**
  * @gridStart indicates the point at which the grid has to start. All the gui's will be realtive to this point
  */
-var gridStart = new Point([600 - ((width * multiplier + axisY.x * height) / 2), 50]);
+var gridStart = new Point([600 - ((width * multiplier + axisY.x * height) / 2), 100]);
 
 
 updateVis = function () {
 
-    gridStart = new Point([600 - ((width * multiplier + axisY.x * height) / 2), 50]);
+    gridStart.x = 600 - ((width * multiplier + axisY.x * height) / 2);
 
     crosshatch.project.clear();
 
@@ -52,6 +52,24 @@ updateVis = function () {
         path.add(p1, p2, p3);
         paths.push(path);
     }
+
+    var path = new Path();
+    p1 = gridStart;
+    p2 = axisZ * layers * layerSeparation + gridStart;
+    path.add(p1, p2);
+    paths.push(path);
+
+    var path = new Path();
+    p1 = axisY * height + gridStart;
+    p2 = axisY * height + axisZ * layers * layerSeparation + gridStart;
+    path.add(p1, p2);
+    paths.push(path);
+
+    var path = new Path();
+    p1 = axisX * width + axisY * height + gridStart;
+    p2 = axisX * width + axisY * height + axisZ * layers * layerSeparation + gridStart;
+    path.add(p1, p2);
+    paths.push(path);
 
     for (var i = 0; i < rows; i++) {
         var path = new Path();
@@ -78,22 +96,109 @@ updateVis = function () {
     });
 
     //----grawing gui items------------------------------------------------------
+    var guiItems = [];
+    var textItems = [];
+
     var path1 = new Path();
     var g1 = new Point(gridStart.x, gridStart.y - 20);
     var c1 = new Point(gridStart.x + width * multiplier, gridStart.y - 20);
     path1.add(g1, c1); //top line, width scroll
+    guiItems.push(path1);
 
-    var path2 = new Path();
+    var text = new PointText((c1 + g1) / 2 - new Point(0, 10)); //descriptive width text
+    text.fontSize = 20;
+    text.content = 'Width: ' + width;
+    text.justification = 'center',
+            textItems.push(text);
+
+    var text = new PointText((c1 + g1) / 2 - new Point(-50, 30)); //Add row text
+    text.content = '+';
+    text.fontSize = 20;
+    text.justification = 'center';
+    text.onMouseUp = function () {
+        if (rows < 10) {
+            rows += 1;
+        }
+        updateVis();
+    };
+    textItems.push(text);
+
+    text = new PointText((c1 + g1) / 2 - new Point(0, 30)); //descriptive row text
+    text.content = 'Rows: ' + rows;
+        text.fontSize = 20;
+    text.justification = 'center';
+    textItems.push(text);
+
+    text = new PointText((c1 + g1) / 2 - new Point(50, 30)); //Remove row text
+    text.content = '-';
+    text.fontSize = 20;
+    text.justification = 'center';
+    text.onMouseUp = function () {
+        if (rows > 2) {
+            rows -= 1;
+        }
+        updateVis();
+    };
+    textItems.push(text);
+
+    var path2 = new Path(); //right line, height scroll 
     g1 = new Point(gridStart.x + 20 + width * multiplier, gridStart.y);
     var c3 = new Point(gridStart.x + 20 + width * multiplier + height * 0.707 * multiplier, gridStart.y + height * 0.707 * multiplier);
-    path2.add(g1, c3); //right line, height scroll 
+    path2.add(g1, c3); 
+    guiItems.push(path2);
+
+    text = new PointText((c3 + g1) / 2 + new Point(10, -10)); //descriptive height text
+    text.fontSize = 20;
+    text.content = 'Height: ' + height;
+    text.justification = 'center';
+    text.rotate(45);
+    textItems.push(text);
+    
+    text = new PointText((c3 + g1) / 2 + new Point(75, 20)); //add column text
+    text.content = '+';
+    text.fontSize = 30;
+    text.justification = 'center';
+    text.rotate(45);
+    text.onMouseUp = function () {
+        if (cols < 10) {
+            cols += 1;
+        }
+        updateVis();
+    };
+    textItems.push(text);
+
+    text = new PointText((c3 + g1) / 2 + new Point(30, -30)); //descriptive column text
+    text.content = 'Columns: ' + cols;
+    text.fontSize = 20;
+    text.justification = 'center';
+    text.rotate(45);
+    textItems.push(text);
+
+    text = new PointText((c3 + g1) / 2 + new Point(-15, -75)); //remove column text
+    text.content = '-';
+    text.fontSize = 30;
+    text.justification = 'center';
+    text.rotate(45);
+    text.onMouseUp = function () {
+        if (cols > 2) {
+            cols -= 1;
+        }
+        updateVis();
+    };
+    textItems.push(text);
 
     var path3 = new Path();
     g1 = new Point(gridStart.x - 20, gridStart.y);
     var c2 = new Point(gridStart.x - 20, gridStart.y + layers * layerSeparation * multiplier);
     path3.add(g1, c2); //left line, layer number
+    guiItems.push(path3);
 
-
+    var text3 = new PointText((c2 + g1) / 2 - new Point(-20, 0));
+    text3.fontSize = 20;
+    text3.content = 'Layers: ' + (layers + 1);
+    text3.justification = 'right',
+            text3.rotate(270);
+    textItems.push(text3);
 
     var circleWidth = new crosshatch.Path.Circle({center: c1, radius: 7});
     circleWidth.fillColor = "blue";
@@ -104,7 +209,7 @@ updateVis = function () {
         this.fillColor = "blue";
     };
     circleWidth.onMouseDrag = function (event) {
-        if (width > 0 && width <= 120) {
+        if (width > 0 && width < 120) {
             if (event.delta.x > 0) {
                 this.position.x += 1;
                 width += 1;
@@ -121,13 +226,15 @@ updateVis = function () {
                 width += 1;
                 updateVis();
             }
-            if (width > 120 && event.delta.x < 0) {
+            if (width >= 120 && event.delta.x < 0) {
                 this.position.x -= 1;
                 width -= 1;
                 updateVis();
             }
         }
     };
+    guiItems.push(circleWidth);
+
 
     var circleHeight = new crosshatch.Path.Circle({center: c3, radius: 7});
     circleHeight.fillColor = "blue";
@@ -139,7 +246,7 @@ updateVis = function () {
         this.fillColor = "blue";
     };
     circleHeight.onMouseDrag = function (event) {
-        if (height > 0 && height <= 120) {
+        if (height > 0 && height < 120) {
             if (event.delta.x > 0) {
                 this.position += 1;
                 height += 1;
@@ -156,15 +263,18 @@ updateVis = function () {
                 height += 1;
                 updateVis();
             }
-            if (height > 120 && event.delta.x < 0) {
+            if (height >= 120 && event.delta.x < 0) {
                 this.position -= 1;
                 height -= 1;
                 updateVis();
             }
         }
     };
+    guiItems.push(circleHeight);
 
-    var circleLayersMore = new crosshatch.Path.Circle({center: c2, radius: 7});
+
+    var circleLayersMore = new crosshatch.Path.RegularPolygon(c2 - new Point(0, 10), 3, -10);
+    circleLayersMore.closed = "true";
     circleLayersMore.fillColor = "blue";
     circleLayersMore.onMouseEnter = function () {
         this.fillColor = "red";
@@ -176,8 +286,10 @@ updateVis = function () {
         layers += 1;
         updateVis();
     };
+    guiItems.push(circleLayersMore);
 
-    var circleLayersLess = new crosshatch.Path.Circle({center: g1, radius: 7});
+
+    var circleLayersLess = new crosshatch.Path.RegularPolygon(g1 + new Point(0, 10), 3, 10);
     circleLayersLess.fillColor = "blue";
     circleLayersLess.onMouseEnter = function () {
         this.fillColor = "red";
@@ -191,11 +303,16 @@ updateVis = function () {
             updateVis();
         }
     };
+    guiItems.push(circleLayersLess);
 
     var layer2 = new crosshatch.Layer({
-        children: [path1, path2, path3, circleWidth, circleHeight, circleLayersMore, circleLayersLess],
+        children: guiItems,
         strokeColor: 'blue',
         strokeWidth: 2
+    });
+
+    var layer3 = new crosshatch.Layer({
+        children: textItems
     });
 
     crosshatch.view.draw();
